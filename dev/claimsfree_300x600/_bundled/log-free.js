41550c80eb94c565ec9280f3d901c39c9b3489d3
Gar <gar@lonelydatu.com>
2018-01-15 13:55:51 -0500

    sdfsdf

--- dev/claimsfree_300x600/_bundled/log-free.js
+++ dev/claimsfree_300x600/_bundled/log-free.js
@@ -15,18 +15,19 @@ function start() {
 
     var tl = new TimelineMax();
     tl.set('.frame1', { opacity: 1 });
-    tl.from(['.logo'], .3, { opacity: 0 });
 
-    tl.add('lines', '+=.3');
-    tl.from('.line.right', .5, { clip: 'rect(0px,' + line.w + 'px,137px,' + line.w + 'px)', ease: Power2.easeOut }, 'lines');
+    tl.add('lines');
+    tl.from('.line.right', .6, { clip: 'rect(0px,' + line.w + 'px,137px,' + line.w + 'px)', ease: Power2.easeOut }, 'lines');
+    tl.from('.line.left', .6, { clip: 'rect(0px,' + 0 + 'px,137px,' + 0 + 'px)', ease: Power2.easeOut }, 'lines');
     tl.from('.house', .3, { opacity: 0, ease: Sine.easeInOut }, '-=.2');
-    // tl.from('.car', .3, {opacity:0, scale:.6, ease:Sine.easeInOut})
-    // tl.from('.car', .6, {x:'-=30'})
-    tl.from('.line.left', .5, { clip: 'rect(0px,' + 0 + 'px,137px,' + 0 + 'px)', ease: Power2.easeOut }, '-=.1');
-    tl.from('.t1', .3, { opacity: 0 }, '+=0');
+    if (document.querySelector('.car')) {
+        tl.from('.car', .3, { opacity: 0, scale: .6, ease: Sine.easeInOut });
+        tl.from('.car', .6, { x: '-=30' });
+    }
+
+    tl.from('.t1', .3, { opacity: 0 }, '-=.1');
 
     tl.add('f2', '+=' + f2Delay);
-
     tl.to('.line.right', .4, { clip: 'rect(0px,' + line.w + 'px,137px,' + line.w + 'px)' }, 'f2');
     tl.to('.line.left', .4, { clip: 'rect(0px,' + 0 + 'px,137px,' + 0 + 'px)' }, 'f2');
     tl.to('.t1', .3, { opacity: 0 }, 'f2+=.2');
